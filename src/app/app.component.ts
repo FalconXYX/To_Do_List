2d8c5682f86ff6f3341f70b59a85112a28b6b1b1
Parth-Mac <parthkj6@gmail.com>
2022-05-06 13:33:17 -0400

    add styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'To_Do_List';
+  todoArray: string[] = [];
+  addTodo(value: any) {
+    if (value != '') {
+      this.todoArray.push(value);
+    }
+  }
 }
