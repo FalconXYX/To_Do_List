4f9d285d9d331614590392d6bd81c5c65952621d
Parth-Mac <parthkj6@gmail.com>
2022-05-06 15:09:25 -0400

    thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
